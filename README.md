ac0fd53f7631b115e17c232286688360a3ab4000
Feroj <feroj21@gmail.com>
2016-07-18 17:42:34 +0600

    Basic angular 2 structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# Starter is basic structure of angular 2 application
+
+### Installation
+
+* Install Node.js
+* Pull the repository
+* Enter directory and run `npm install`
+* Run `npm start`
---
+++ app/components/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'starter-app',
+    templateUrl: 'app/components/app/app.component.html'
+})
+export class AppComponent {}
