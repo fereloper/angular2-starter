6f8b62c82feceb5bb627b94dd0361adf7c67a274
Feroj <feroj21@gmail.com>
2016-07-19 12:46:57 +0600

    Added styleUrls in app component

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'starter-app',
-    templateUrl: 'app/components/app/app.component.html'
+    selector: 'starter-app', // DOM selector name
+    templateUrl: 'app/components/app/app.component.html', //Place your template url of this component
+    styleUrls:['app/components/app/app.component.css'] //Place your style urls of this component
 })
 export class AppComponent {}
